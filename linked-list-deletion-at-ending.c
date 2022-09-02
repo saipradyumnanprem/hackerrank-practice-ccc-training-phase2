092615f308e1fbb5b9379b53ab89dcba6a93bc08
Sai Pradyumnan Prem <72010881+saipradyumnanprem@users.noreply.github.com>
2022-09-02 14:42:36 +0530

    Create linked-list-deletion-at-ending.c

---
+++ linked-list-deletion-at-ending.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node * next;
+};
+
+struct node * head = NULL;
+
+void insb(int x)
+{
+    struct node *nn = (struct node *) malloc(sizeof(struct node));
+    nn -> data = x;
+    nn -> next = head;
+    head = nn;
+}
+
+void insend(int x)
+{
+    if (!head){ insb(x); return;}
+    struct node *t = head, *nn = (struct node *) malloc(sizeof(struct node));
+    nn -> data = x;
+    nn -> next = NULL;
+    while (t -> next)   t = t-> next;
+    t -> next = nn;
+        
+}
+
+void delbeg(){
+    if (head){
+        struct node * t = head;
+        head = head -> next;
+        free(t);
+    }
+}
+
+void delend(){
+    if (head){
+        if (!head -> next) {delbeg(); return;}
+    
+    struct node *p = head, *t;
+    while (p -> next -> next) p = p->next;
+    t = p -> next;
+    p -> next = NULL;
+    free(t);
+    }    
+}
+
+void display()
+{
+    struct node *t = head;
+    while(t){
+        printf("%d", t->data);
+        
+        if (t -> next != NULL){
+            printf("->");
+        }
+        t = t->next;
+    }
+    
+}
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int n,i,d;
+    scanf("%d", &n);
+    while(n--){
+        scanf("%d", &i);
+        insend(i);
+    }
+    
+    scanf("%d", &d);
+    while(d--)
+        delend();
+    
+    display();
+    return 0;
+}
